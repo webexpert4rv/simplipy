be0fb363bc032b2c6c18fcd2bd33118aa9b07fae
user <webexpert4rv@gmail.com>
2017-10-07 11:07:53 +0530

    work on style li

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                      {{--  <li><a href="{{ url('admin') }}">Admin Login</a></li>
-                            <li><a href="{{ route('login') }}">User Login</a></li>--}}
+                        <li style="padding-bottom: 50px;">{{--<a href="{{ url('admin') }}">Admin Login</a></li>
+                            <li><a href="{{ route('login') }}">User Login</a>--}}</li>
                             {{--<li><a href="{{ route('register') }}">Register</a></li>--}}
                         @else
                             <li class="dropdown">
