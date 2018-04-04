eb4b61ce5d2a46aa1107f72816e35b7e2a43a3e6
user <webexpert4rv@gmail.com>
2018-04-04 14:10:15 +0530

    work on message email template space issue

--- resources/views/emails/message_report.blade.php
+++ resources/views/emails/message_report.blade.php
@@ -30,77 +30,77 @@
         <br/>
     @endif
 </div>
-<table name="patient_table" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" class="patient_data">
+<table name="patient_table" border="0" cellpadding="0" cellspacing="0" height="100%" class="patient_data">
     @if(isset($civil_id))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Civilité:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$civil_id }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Civilité:</td>
+            <td style="padding-top: 15px;">{{ @$civil_id }}</td>
         </tr>
     @endif
     @if(isset($name))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Nom:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$name }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Nom:</td>
+            <td style="padding-top: 15px;">{{ @$name }}</td>
         </tr>
     @endif
     @if(isset($last_name))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Prénom:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$last_name }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Prénom:</td>
+            <td style="padding-top: 15px;">{{ @$last_name }}</td>
         </tr>
     @endif
     @if(isset($dob))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Date de naissance:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$dob }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Date de naissance:</td>
+            <td style="padding-top: 15px;">{{ @$dob }}</td>
         </tr>
     @endif
     @if(isset($address))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Adresse postale:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$address }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Adresse postale:</td>
+            <td style="padding-top: 15px;">{{ @$address }}</td>
         </tr>
     @endif
     @if(isset($complete_address))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Complément d'adresse:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$complete_address }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Complément d'adresse:</td>
+            <td style="padding-top: 15px;">{{ @$complete_address }}</td>
         </tr>
     @endif
     @if(isset($mobile))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Numéro de téléphone:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$mobile }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Numéro de téléphone:</td>
+            <td style="padding-top: 15px;">{{ @$mobile }}</td>
         </tr>
     @endif
     @if(isset($fax))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Numéro de fax:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$fax }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Numéro de fax:</td>
+            <td style="padding-top: 15px;">{{ @$fax }}</td>
         </tr>
     @endif
     @if(isset($to_email))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Adresse mail:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$to_email }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Adresse mail:</td>
+            <td style="padding-top: 15px;">{{ @$to_email }}</td>
         </tr>
     @endif
     @if(isset($security))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Numéro de sécurité sociale:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$security }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Numéro de sécurité sociale:</td>
+            <td style="padding-top: 15px;">{{ @$security }}</td>
         </tr>
     @endif
     @if(isset($exam))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Type d'examen:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$exam }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Type d'examen:</td>
+            <td style="padding-top: 15px;">{{ @$exam }}</td>
         </tr>
     @endif
     @if(isset($dateexam))
         <tr>
-            <td width="20%" style="padding-top: 15px;font-weight:bold;">Date de l'examen:</td>
-            <td width="80%" style="padding-top: 15px;">{{ @$dateexam }}</td>
+            <td style="padding-top: 15px;font-weight:bold;">Date de l'examen:</td>
+            <td style="padding-top: 15px;">{{ @$dateexam }}</td>
         </tr>
     @endif
     <tr>
